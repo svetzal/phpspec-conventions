526240b33d9cc4137eeca99b66a13c3385669612
Stacey Vetzal <stacey@vetzal.com>
2015-08-11 12:51:24 -0400

    adding some actual source files

---
+++ src/Extension.php
@@ -0,0 +1,16 @@
+<?php
+
+use PhpSpec\ServiceContainer;
+
+class Extension implements ExtensionInterface {
+
+    public function load(ServiceContainer $container) {
+        $this->setupLocators($container);
+    }
+
+    private function setupLocators(\PhpSpec\ServiceContainer $container) {
+        $locatorAssembler = new LocatorAssembler();
+        $locatorAssembler->build($container);
+    }
+
+}
